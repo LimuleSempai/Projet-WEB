fd41ba66737e6197253ba54c1410a157675b6fb7
OYama1909 <125834754+OYama1909@users.noreply.github.com>
2023-11-09 13:47:57 +0100

    fichier js pour l'index fonctionnel

--- script/script_index.js
+++ script/script_index.js
@@ -1,42 +1,38 @@
-// Charger le fichier JSON
-fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        const hamburger = document.getElementById('hamburger');
-        const nav_ul = document.getElementById('nav_ul');
-        const progressBar = document.getElementById('progressBar');
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.getElementById("hamburger");
+    const nav_ul = document.getElementById("nav_ul");
+    const progressBar = document.getElementById("progressBar");
 
-        hamburger.onclick = () => {
-            hamburger.classList.toggle("open");
-            nav_ul.classList.toggle("slide");
+    // Toggle hamburger et nav_ul
+    hamburger.onclick = () => {
+        hamburger.classList.toggle("open");
+        nav_ul.classList.toggle("slide");
+    };
+
+    // Gérer la barre de progression
+    window.onscroll = () => {
+        const page = document.documentElement;
+        let totalHeight = page.scrollHeight;
+        let visibleHeight = page.clientHeight;
+        let scrolling = page.scrollTop;
+        let max = page.scrollHeight - page.clientHeight;
+        progressBar.style.width = Math.floor(scrolling / max * 100) + "%";
+
+        if (progressBar.style.width === "100%") {
+        progressBar.style.backgroundColor = "gray";
+        } else {
+        progressBar.style.backgroundColor = "rgb(255, 255, 255)";
         }
+    };
+});
 
-        window.onscroll = () => {
-            const page = document.documentElement;
-            // let totalHeight = page.scrollHeight;
-            // let visibleHeight = page.clientHeight;
-            let scrolling = page.scrollTop;
-            let max = page.scrollHeight - page.clientHeight;
-            progressBar.style.width = Math.floor(scrolling / max * 100) + "%";
+document.addEventListener('DOMContentLoaded', function() {
+    const bouton = document.getElementById('changerCouleur');
 
-            if (progressBar.style.width === "100%")
-            progressBar.style.backgroundColor = data.progressBarCompleteColor;
-            else
-            progressBar.style.backgroundColor = data.progressBarDefaultColor;
-        }
-    })
-
-
-    document.addEventListener('DOMContentLoaded', function() {
-        const bouton = document.getElementById('changerCouleur');
-
-        bouton.addEventListener('click', function() {
-            const header = document.getElementById('header');
-            const body = document.getElementById('body');
-            header.style.backgroundColor = '#28a745';
-            body.style.backgroundColor = '#28a745';
-        });
+    bouton.addEventListener('click', function() {
+        const header = document.getElementById('header');
+        const body = document.getElementById('body');
+        header.style.backgroundColor = '#28a745';
+        body.style.backgroundColor = '#28a745';
     });
-
-
-
+});
