c3d8ce530fe28f59086c932b28077a1aecec0dff
OYama1909 <125834754+OYama1909@users.noreply.github.com>
2023-11-09 22:43:29 +0100

    enlevage du fichier abour_us.js qui sert à rien

--- script/about_us.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('copyButton').addEventListener('click', function() {
-    var discordId = document.getElementById('discordId');
-    discordId.select();
-    document.execCommand('copy');
-    alert('Identifiant Discord copié : ' + discordId.value);
-  });
-  
